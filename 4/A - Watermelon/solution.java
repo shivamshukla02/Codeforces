1c86366dabfd7ace59338efa01a0906ecc8688c9
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-15 14:05:02 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int w = sc.nextInt();
+if (w > 2 && w % 2 == 0) {
+    System.out.print("Yes");
+} else {
+    System.out.print("No");
+}}}
