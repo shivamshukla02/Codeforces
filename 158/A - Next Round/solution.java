024a64664b288ffa322493510b8b924c6ed3063e
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-17 09:07:14 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int a[]=new int [n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();}
+            int c=0;
+            for(int i=0;i<n;i++){
+                if(a[i]>=a[k-1]&&a[i]>0){
+                    c++;}}
+                    System.out.print(c);}}
