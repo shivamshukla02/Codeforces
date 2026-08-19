dbebff17c650c7c3456ea654ee4ab3cfe18c04c0
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-19 22:29:18 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int y=0;
+        while(a<=b){
+            a=3*a;
+            b=2*b;
+            y++;}
+            System.out.print(y);}}
