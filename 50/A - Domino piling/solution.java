f4cf10447a26dc6853cad975283c427f07b19f4f
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-27 23:22:50 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        System.out.print((M*N)/2);}}
