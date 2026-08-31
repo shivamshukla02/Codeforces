a6aa7656c6589cd4dc8997aaba7a132bcb2725a7
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-08-31 23:09:55 +0530

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                result.append('0');
+            } else {
+            result.append('1');}}
+System.out.println(result.toString());}}
