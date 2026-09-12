55383152873833e1d25a5069e07d6eb3b40986c5
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-09-12 20:31:38 +0530

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+            System.out.print("25");
+        }}
+ 
