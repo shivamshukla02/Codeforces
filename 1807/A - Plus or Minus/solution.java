412381f83670f484468867f67b555947d9eab052
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-09-16 10:41:24 +0530

    Add Plus or Minus [A] from Codeforces

---
+++ 1807/A - Plus or Minus/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a+b == c){
+                System.out.println("+");}
+            else {
+                System.out.println("-");}}
+        }}
