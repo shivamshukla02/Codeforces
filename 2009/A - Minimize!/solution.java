c71fb0b9ceddb0af29f2c2ae88e585c78d331ac7
Shivam Shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-09-22 19:44:06 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+     Scanner sc = new Scanner(System.in);
+     int t = sc.nextInt();
+     while(t-->0){
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         System.out.println(b-a);}
+         }
+     }
